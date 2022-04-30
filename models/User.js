e1915aee292bc14ef53cbf152073cf756619b2d5
Skronkie <ebrockway01@gmail.com>
2022-04-29 22:47:39 -0500

    intial commit

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+//  user model
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+});
