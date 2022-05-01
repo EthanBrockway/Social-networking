6cf0984fc663d2e709fc0ae9f02ac5a796560c0f
Skronkie <ebrockway01@gmail.com>
2022-04-30 21:00:02 -0500

    created all of the UserSchema model, i think

--- models/User.js
+++ models/User.js
@@ -1,11 +1,43 @@
-// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-// //  user model
-// const userSchema = new Schema({
-//   username: {
-//     type: String,
-//     unique: true,
-//     required: true,
-//     trim: true,
-//   },
-// });
+//  user model
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+module.exports = User;
