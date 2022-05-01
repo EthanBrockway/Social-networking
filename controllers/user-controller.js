21027c439a59bd842a99edf1e32fc999f96844c2
Skronkie <ebrockway01@gmail.com>
2022-04-30 21:07:20 -0500

    added start of user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,13 @@
+const { User } = require("../models.");
+
+const userController = {
+  // finds all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
