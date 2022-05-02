3d5d310ced57eafac46d810ba040ab70b481a411
Skronkie <ebrockway01@gmail.com>
2022-05-01 21:00:50 -0500

    added update thought and redid the post controller for creating a thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -22,7 +22,7 @@ const thoughtController = {
   postThoughts({ body }, res) {
     Thought.create(body)
       .then((dbThoughtData) => {
-        User.findOneAndUpdate(
+        return User.findOneAndUpdate(
           {
             _id: body.userId,
           },
@@ -30,6 +30,23 @@ const thoughtController = {
             $push: { thoughts: dbThoughtData._id },
           }
         );
+      })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  updateThought({ params, body }, res) {
+    Thought.findByIdAndUpdate({ _id: params.thoughtId }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
         res.json(dbThoughtData);
       })
       .catch((err) => {
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,9 +3,10 @@ const {
   getAllThoughts,
   postThoughts,
   getThoughtById,
+  updateThought,
 } = require("../../controllers/thought-controller.js");
 
 router.route("/").get(getAllThoughts).post(postThoughts);
-router.route("/:thoughtId").get(getThoughtById);
+router.route("/:thoughtId").get(getThoughtById).put(updateThought);
 
 module.exports = router;
