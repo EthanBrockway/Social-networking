3d1dbd3768ae382829bdf06e69f8ee96e9dd6c05
Skronkie <ebrockway01@gmail.com>
2022-05-01 23:52:52 -0500

    couldnt finish removeReaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -103,6 +103,30 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      {
+        $pull: {
+          reactions: {
+            reactionId: params.reactionId,
+          },
+        },
+      },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No reaction found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -6,11 +6,13 @@ const {
   updateThought,
   removeThought,
   addReaction,
+  removeReaction,
 } = require("../../controllers/thought-controller.js");
 
 router.route("/").get(getAllThoughts).post(postThoughts);
 router.route("/:thoughtId").get(getThoughtById).put(updateThought);
 router.route("/:thoughtId/:userId").delete(removeThought);
 router.route("/:thoughtId/reaction").post(addReaction);
+router.route("/:thoughtId/reaction/:reactionId").delete(removeReaction);
 
 module.exports = router;
