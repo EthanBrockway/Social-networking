2771eccb64689434945ebf67f66bc879349408a0
Skronkie <ebrockway01@gmail.com>
2022-05-01 20:13:16 -0500

    added git route for single thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,6 +4,21 @@ const thoughtController = {
   getAllThoughts(req, res) {
     Thought.find({}).then((dbThoughtData) => res.json(dbThoughtData));
   },
+  getThoughtById({ params }, res) {
+    console.log({ params });
+    Thought.findOne({ _id: params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   postThoughts({ body }, res) {
     Thought.create(body)
       .then((dbThoughtData) => {
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const {
   getAllThoughts,
   postThoughts,
+  getThoughtById,
 } = require("../../controllers/thought-controller.js");
 
 router.route("/").get(getAllThoughts).post(postThoughts);
+router.route("/:thoughtId").get(getThoughtById);
 
 module.exports = router;
