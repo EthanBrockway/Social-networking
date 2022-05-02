fa67e0dfb306d0f02b7f7bbaf7445050b9d82fe8
Skronkie <ebrockway01@gmail.com>
2022-05-01 21:34:03 -0500

    added removeFriend controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -82,6 +82,28 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  removeFriend({ params }, res) {
+    User.findByIdAndUpdate(
+      { _id: params.userId },
+      {
+        $pull: {
+          friends: params.friendId,
+        },
+      },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,10 +6,11 @@ const {
   updateUser,
   deleteUser,
   addFriend,
+  removeFriend,
 } = require("../../controllers/user-controller");
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
-router.route("/:userId/friends/:friendId").post(addFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
